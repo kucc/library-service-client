8d45b53a86df3bc40378f586190273314c3d779d
cherry <sag06078@korea.ac.kr>
2023-07-05 15:33:15 +0900

    Feat: 검색결과 페이지 모양 완성

---
+++ src/components/items/SearchCard.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import DefaultBtn from "./DefaultBtn";
+
+const CardBody = (props) => {
+    const { title, author, publisher, publication_year } = props;
+    return (
+        <div>
+            <div className="px-[24px] text-xl">{title}</div>
+            <div className="flex divide-x text-base text-gray-400">
+                <div className="px-[24px]">{author}</div>
+                <div className="px-[24px]">{publisher}</div>
+                <div className="px-[24px]">{publication_year}</div>
+            </div>
+        </div>
+    )
+}
+
+const CardFooter = (props) => {
+    const { footerProps } = props;
+    return (
+        <div className="flex flex-wrap w-[120px]">
+            {footerProps.map((e, i) => (
+                <dic className="h-[53px]" key={i}>
+                    <DefaultBtn text={e.text} handleClick={e.handleClick} />
+                </dic>
+            ))}
+        </div>
+    )
+}
+
+const Card = (props) => {
+    const { img, bodyProps, footerProps } = props;
+    return (
+        <div className="border-2 rounded-2xl py-7 px-16 m-2 bg-gray-50 flex w-100 h-[150px] justify-between">
+            <div className="flex">
+                <div className="my-auto">
+                    <Image src={img} alt="card img" width={90} height={90} />
+                </div>
+                <div className="my-auto ml-5">
+                    <CardBody {...bodyProps} />
+                </div>
+            </div>
+            <CardFooter footerProps={footerProps} />
+        </div>
+    )
+};
+
+export default Card;
+
--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -2,22 +2,93 @@ import logo from '../../img/logo512.png'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import RightArrow from '@components/components/items/RightArrow'
+import Option from '@components/components/items/Option'
+import imgbook from "../../img/Ellipse.png";
+import LeftArrow from '@components/components/items/LeftArrow'
+import SearchCard from '@components/components/items/SearchCard'
+import BoardTitle from '@components/components/Board/BoardTitle'
 
 const Search = () => {
     const router = useRouter()
     const { query } = router
+    console.log(query.bookname)
+    const category = ["카테고리", "전체", "인공지능", "데이터", "디자인", "게임", "하드웨어", "수학", "이론", "웹", "앱", "언어", "기타"];
+    const sort = ["정렬", "제목순", "등록일순", "출판일순", "인기순", "평점순F"];
+    const book = [
+        {
+            img: imgbook,
+            title: "test1",
+            author: "cherry",
+            publisher: "hanbit",
+            publication_year: "2022",
+        },
+        {
+            img: imgbook,
+            title: "test2",
+            author: "cherry",
+            publisher: "hanbit",
+            publication_year: "2022",
+        },
+        {
+            img: imgbook,
+            title: "test1",
+            author: "cherry",
+            publisher: "hanbit",
+            publication_year: "2022",
+        },
+        {
+            img: imgbook,
+            title: "test2",
+            author: "cherry",
+            publisher: "hanbit",
+            publication_year: "2022",
+        },
+        {
+            img: imgbook,
+            title: "test1",
+            author: "cherry",
+            publisher: "hanbit",
+            publication_year: "2022",
+        },
+    ];
 
     return (
         <>
-            <p>Search: {query.bookname}</p>
-            <Link href="detail/asdf">
-                <div style={{display:"flex"}}>
-                    <Image src={logo} alt="test" width={500} />
-                    <div>
-                        <h1>책이름</h1>
+            <div className="w-[960px] m-auto mb-[200px]">
+                <div className="mt-[53px]">
+                    <Link href="/">
+                        <LeftArrow />
+                    </Link>
+                </div>
+                <div className='flex justify-between mt-[64px] mb-[22px]'>
+                    <BoardTitle title="검색 결과" className="" />
+                    <div className='flex w-[310px] justify-between'>
+                        <div className=''>
+                            <Option selectOptionList={category} />
+                        </div>
+                        <div className=''>
+                            <Option selectOptionList={sort} />
+                        </div>
                     </div>
                 </div>
-            </Link>
+                <div>
+                    {book.map((e, i) => (
+                        <SearchCard
+                            key={i}
+                            img={e.img}
+                            bodyProps={e}
+                            footerProps={[
+                                { text: "대출", handleClick: () => handleClick("대출") },
+                            ]}
+                        />
+                    ))}
+                </div>
+                <div className='flex mx-auto'>
+                    <LeftArrow />
+                    <RightArrow />
+                </div>
+            </div>
         </>
     )
 }
