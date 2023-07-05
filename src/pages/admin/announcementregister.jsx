88ed8be0166690f708749ae9f92b7f88cd9680a4
manu1307 <jasonsc@naver.com>
2023-07-05 16:12:39 +0900

    remove : admin 폴더 삭제

--- src/pages/admin/announcementregister.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Announcementregister = () => {
-  return <div>Announcementregister</div>;
-};
-
-export default Announcementregister;
--- src/pages/admin/bookregister.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Bookregister = () => {
-  return <div>Bookregister</div>;
-};
-
-export default Bookregister;
